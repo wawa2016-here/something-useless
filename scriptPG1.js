7e5124917f281da354207b1ca9d0cee4f652d4ff
Wawa2016-here <lemver2016@outlook.com>
2026-07-26 19:05:40 -0500

    Create scriptPG1.js

---
+++ scriptPG1.js
@@ -0,0 +1,4 @@
+const button = document.getElementById("some-changes-to-the-title");
+button.addEventListener('click', () => {
+    element.insertAdjacentHTML('beforeend', 'ok so I'm back with some data done Im saying nothing')
+  });
