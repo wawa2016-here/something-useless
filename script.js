00d27753a2b95b822824625e9c0183024aea15c5
Wawa2016-here <lemver2016@outlook.com>
2026-07-31 16:15:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const button = document.getElementById("some-changes-to-the-title");
+
 button.addEventListener('click', () => {
-    document.getElementById("title")innerHTML = "Hello this is useless so don't blame me for wasting your time I have news no news"
-  });
+  document.getElementById("title").innerHTML = "Hello this is useless so don't blame me for wasting your time I have news no news";
+});
