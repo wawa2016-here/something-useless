0b8f138d567b61474a7c2b325df98bf785c41633
Wawa2016-here <lemver2016@outlook.com>
2026-07-31 16:42:23 -0500

    Create PG-two-script.js

---
+++ PG-two-script.js
@@ -0,0 +1,5 @@
+const button = document.getElementById("a-secret");
+
+button.addEventListener('click', () => {
+  document.getElementById("title").innerHTML = "No secrets I have wasted your time agian";
+});
